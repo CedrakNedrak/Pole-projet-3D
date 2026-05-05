813c26e6bc226f57ca120dbbc8e74d8778194ba1
Nathanael Tousch <nathanael.tousch@cs-student.fr>
2026-05-05 14:33:22 +0200

    Added empty Soldier Spawning function

---
+++ Assets/Fonctionnality/Town/Script/PurchaseSoldierButton.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PurchaseSoldierButton : MonoBehaviour
+{
+    [SerializeField] private Ressources ressources;
+    [SerializeField] private GameObject soldier;
+    [SerializeField] private int soldierPrice = 20;
+    private Vector3 spawnPosition = new Vector3(0,0,0);
+
+    public void PurchaseSoldier()
+    {
+        
+    }
+
+}
+
