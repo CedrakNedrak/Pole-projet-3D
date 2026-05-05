884319d76482687a33b0cab6f6b7d5fe3975cee9
pro2p <pierre.fessart@gmail.com>
2026-05-05 16:35:59 +0200

    cleaner gameplayManager
    spawn town function

--- Assets/Fonctionnality/Managers/GameplayManager.cs
+++ Assets/Fonctionnality/Managers/GameplayManager.cs
@@ -11,12 +11,14 @@ public class GameplayManager : MonoBehaviour
     {
         //comment générer une ville
         //rq: ville de niveau 0 = ruine
-        TownData firstTown = Instantiate(townPrefab, new Vector3(-2.883264f, -1.516974f, 0.4262036f), Quaternion.identity);
-        firstTown.InitializeReferences(townUICanvas, townUI, camera);
-        firstTown.Initialize("Cosy Cave", 200, 70, 1, 0, 40, 1000);
+        SpawnTown(new Vector3(-2.883264f, -1.516974f, 0.4262036f), Quaternion.identity, "Cosy Cave", 200, 70, 1, 0, 40, 1000);
+        SpawnTown(new Vector3(-8.69502f, -1.45207f, 4.136147f), Quaternion.identity, "Miner Haven", 0, 40, 0, 200, 40, 1000);
+    }
 
-        TownData firstRuin = Instantiate(townPrefab, new Vector3(-8.69502f, -1.45207f, 4.136147f), Quaternion.identity);
-        firstRuin.InitializeReferences(townUICanvas, townUI, camera);
-        firstRuin.Initialize("Miner Haven", 0, 40, 0, 200, 40, 1000);
+    public void SpawnTown(Vector3 position, Quaternion rotation, string name, int startGold, int goldIncome, int townLevel, int buildPrice, int minerPrice, int upgradePrice, string initialTownName = "Abandoned Ruin")
+    {
+        TownData newTown = Instantiate(townPrefab, position, rotation);
+        newTown.InitializeReferences(townUICanvas, townUI, camera);
+        newTown.Initialize(name, startGold, goldIncome, townLevel, buildPrice, minerPrice, upgradePrice, initialTownName);
     }
 }
