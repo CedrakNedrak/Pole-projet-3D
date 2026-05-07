6ef6a38e838ebce1a1ce949ae800e3fb25058654
Cedric Vilandrau <cedric.vilandrau@gmail.com>
2026-05-07 10:37:47 +0200

    Premier ajout

--- Assets/Fonctionnality/Town/Script/TownLevelingSystem.cs
+++ Assets/Fonctionnality/Town/Script/TownLevelingSystem.cs
@@ -5,7 +5,8 @@ public class TownLevelingSystem : MonoBehaviour
     [SerializeField] private TownData townData;
     [SerializeField] private int upgradePrice = 5;
     public int UpgradePrice => upgradePrice;
-
+    [SerializeField] GameObject ruinGameObject;
+    [SerializeField] GameObject townLevelOneGameObject;
     [SerializeField] private int townLevel = 1;
     public int TownLevel => townLevel;
 
@@ -14,6 +15,16 @@ public class TownLevelingSystem : MonoBehaviour
         if (townData.ressources.SpendGold(upgradePrice))
         {
             SetTownLevel(townLevel + 1);
+
+            ruinGameObject.SetActive(false); 
+            townLevelOneGameObject.SetActive(false);
+
+            switch (townLevel)
+            {
+                case (0): ruinGameObject.SetActive(true); break;
+                case (1): townLevelOneGameObject.SetActive(true);  break;
+                default : break;
+            }
         }
     }
     public void SetTownLevel(int level)
